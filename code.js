4dc0963c4b5d4960e32678f60c39a53612d79e29
Gamaj <gamajd1@gmail.com>
2019-10-23 09:41:08 -0500

    Version Estable

--- code.js
+++ code.js
@@ -246,9 +246,9 @@ function refreshRentabilidad(){
     ganancia.rows[2].cells[0].innerHTML = Math.round(((tableComparacion.rows[2].cells[0].innerHTML - tableComparacion.rows[2].cells[1].innerHTML ) / tableComparacion.rows[2].cells[1].innerHTML)*100) + "%";
     ganancia.rows[2].cells[1].innerHTML = Math.round(tableComparacion.rows[2].cells[0].innerHTML - tableComparacion.rows[2].cells[1].innerHTML);
     
-    tableComparacion.rows[2].cells[0].innerHTML = insertarFormatoPesos( tableComparacion.rows[2].cells[0].innerHTML);
-    tableComparacion.rows[2].cells[1].innerHTML = insertarFormatoPesos( tableComparacion.rows[2].cells[1].innerHTML);
-    ganancia.rows[2].cells[1].innerHTML = insertarFormatoPesos(ganancia.rows[2].cells[1].innerHTML)
+    tableComparacion.rows[2].cells[0].innerHTML = insertarFormatoPesos2( tableComparacion.rows[2].cells[0].innerHTML);
+    tableComparacion.rows[2].cells[1].innerHTML = insertarFormatoPesos2( tableComparacion.rows[2].cells[1].innerHTML);
+    ganancia.rows[2].cells[1].innerHTML = insertarFormatoPesos2(ganancia.rows[2].cells[1].innerHTML)
     
     
 }
@@ -278,7 +278,7 @@ function addSourceOfVolume() {
         c = "1";
     }else if (c=="Destilado Nacional"){
         c = "2";
-    }else if (c=="Destilaado Importado"){
+    }else if (c=="Destilado Importado"){
         c = "3";
     }
 
@@ -336,7 +336,7 @@ function addSourceOfVolume() {
 
                         cell4.style.backgroundColor = "#EEFFA1";
 
-                        cell5.innerHTML = Math.round(costosource / separado[4]);
+                        cell5.innerHTML = Math.round((costosource / separado[4]) * 100) /100;
 
                         cell6.innerHTML = costosource;
 
@@ -547,7 +547,7 @@ function calcRentabilidad() {
 
         suma = suma + cal;
         
-        table.rows[i].cells[4].innerHTML = insertarFormatoPesos(table.rows[i].cells[4].innerHTML);
+        table.rows[i].cells[4].innerHTML = insertarFormatoPesos2(table.rows[i].cells[4].innerHTML);
         
 
     }
@@ -558,11 +558,11 @@ function calcRentabilidad() {
 
     //  costopro = new Intl.NumberFormat("en-US", { style: 'currency', currency: 'USD' }).format(costopro);
 
-    table.rows[i].cells[2].innerHTML = insertarFormatoPesos(suma);
+    table.rows[i].cells[2].innerHTML = insertarFormatoPesos2(suma);
 
-    table.rows[i + 2].cells[1].innerHTML = insertarFormatoPesos(suma * 0.05);
+    table.rows[i + 2].cells[1].innerHTML = insertarFormatoPesos2(suma * 0.05);
 
-    table.rows[i + 3].cells[1].innerHTML = insertarFormatoPesos(costopro);
+    table.rows[i + 3].cells[1].innerHTML = insertarFormatoPesos2(costopro);
 
     // table.rows[i].cells[4].innerHTML = suma;
 
@@ -582,7 +582,7 @@ function myFunction() {
   //  console.log(table);
     var calc = table.rows[1].cells;
 
-    var cal = Math.round(calc[3].innerHTML / calc[2].innerHTML);
+    var cal = Math.round((calc[3].innerHTML / calc[2].innerHTML)*100)/100;
 
    // console.log(cal);
 
@@ -620,7 +620,7 @@ function myFunction() {
 
         calc = table.rows[i].cells;
 
-        cal = Math.round(calc[3].innerHTML / calc[2].innerHTML);
+        cal = Math.round((calc[3].innerHTML / calc[2].innerHTML)*100) / 100;
 
         cal = insertarFormatoPesos(cal);
 
@@ -720,8 +720,8 @@ function myFunction() {
 
     ingredientes.rows[i + 8].cells[1].innerHTML = Math.round((ingredientes.rows[i + 6].cells[1].innerHTML / precio) * 100) + "%";
     
-    ingredientes.rows[i + 6].cells[1].innerHTML = insertarFormatoPesos(  ingredientes.rows[i + 6].cells[1].innerHTML);
-    ingredientes.rows[i + 5].cells[1].innerHTML = insertarFormatoPesos(  ingredientes.rows[i + 5].cells[1].innerHTML);
+    ingredientes.rows[i + 6].cells[1].innerHTML = insertarFormatoPesos2(  ingredientes.rows[i + 6].cells[1].innerHTML);
+    ingredientes.rows[i + 5].cells[1].innerHTML = insertarFormatoPesos2(  ingredientes.rows[i + 5].cells[1].innerHTML);
     
     
     refreshRentabilidad();
@@ -763,7 +763,7 @@ function reCalcular() {
 
     ingredientes.rows[i + 4].cells[1].style.backgroundColor = "#EEFFA1";
 
-    ingredientes.rows[i + 5].cells[1].innerHTML = insertarFormatoPesos(precio * 0.08);
+    ingredientes.rows[i + 5].cells[1].innerHTML = insertarFormatoPesos2(precio * 0.08);
     var totalCostoProd = retirarFormatoPesos(ingredientes.rows[i + 3].cells[1].innerHTML);
 
     ingredientes.rows[i + 6].cells[1].innerHTML = precio - retirarFormatoPesos(ingredientes.rows[i + 5].cells[1].innerHTML) - totalCostoProd;
@@ -772,7 +772,7 @@ function reCalcular() {
 
     ingredientes.rows[i + 8].cells[1].innerHTML = Math.round((ingredientes.rows[i + 6].cells[1].innerHTML / precio) * 100) + "%";
     
-    ingredientes.rows[i + 6].cells[1].innerHTML = insertarFormatoPesos(  ingredientes.rows[i + 6].cells[1].innerHTML);
+    ingredientes.rows[i + 6].cells[1].innerHTML = insertarFormatoPesos2(  ingredientes.rows[i + 6].cells[1].innerHTML);
     
     refreshRentabilidad();
    
@@ -794,21 +794,21 @@ function calcularRentabilidadSOF() {
     var unidadA = ingredientes.rows[2].cells[1].innerHTML;
     var cantidad = ingredientes.rows[2].cells[3].innerHTML;
    // console.log(unidadA + " " + cantidad) 
-    ingredientes.rows[2].cells[4].innerHTML = Math.round(unidadA / cantidad);
+    ingredientes.rows[2].cells[4].innerHTML = Math.round((unidadA / cantidad)*100)/100;
 
     suma = ingredientes.rows[2].cells[4].innerHTML * ingredientes.rows[2].cells[3].innerHTML;
      ingredientes.rows[2].cells[4].innerHTML =  insertarFormatoPesos(ingredientes.rows[2].cells[4].innerHTML);
-    ingredientes.rows[2].cells[5].innerHTML = insertarFormatoPesos(suma);
+    ingredientes.rows[2].cells[5].innerHTML = insertarFormatoPesos2(suma);
     var costopro = Math.round(suma) + Math.round(suma * 0.05);
 
 
     //  costopro = new Intl.NumberFormat("en-US", { style: 'currency', currency: 'USD' }).format(costopro);
 
-    ingredientes.rows[i].cells[2].innerHTML = insertarFormatoPesos(suma);
+    ingredientes.rows[i].cells[2].innerHTML = insertarFormatoPesos2(suma);
 
-    ingredientes.rows[i + 2].cells[1].innerHTML = insertarFormatoPesos(suma * 0.05);
+    ingredientes.rows[i + 2].cells[1].innerHTML = insertarFormatoPesos2(suma * 0.05);
 
-    ingredientes.rows[i + 3].cells[1].innerHTML = insertarFormatoPesos(costopro);
+    ingredientes.rows[i + 3].cells[1].innerHTML = insertarFormatoPesos2(costopro);
 
     //calcRentabilidad();
 
@@ -820,9 +820,9 @@ function calcularRentabilidadSOF() {
 
     ingredientes.rows[i + 4].cells[1].style.backgroundColor = "#EEFFA1";
 
-    ingredientes.rows[i + 5].cells[1].innerHTML = insertarFormatoPesos(precio * 0.08);
+    ingredientes.rows[i + 5].cells[1].innerHTML = insertarFormatoPesos2(precio * 0.08);
 
-    ingredientes.rows[i + 6].cells[1].innerHTML = insertarFormatoPesos( precio - retirarFormatoPesos(ingredientes.rows[i + 5].cells[1].innerHTML) - costopro);
+    ingredientes.rows[i + 6].cells[1].innerHTML = insertarFormatoPesos2( precio - retirarFormatoPesos(ingredientes.rows[i + 5].cells[1].innerHTML) - costopro);
     
     ingredientes.rows[i + 7].cells[1].innerHTML = Math.round((costopro/precio)*100)+ "%";
     
@@ -990,10 +990,17 @@ function getIdProd(nombre) {
 //https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/NumberFormat
 function insertarFormatoPesos(numero){
    var retorno = new Intl.NumberFormat("en-US", { style: 'currency', currency: 'USD' }).format(numero);
+ //  retorno = retorno.split(".");
+   //console.log(retorno);
+   return retorno;
+    
+}
+
+function insertarFormatoPesos2(numero){
+   var retorno = new Intl.NumberFormat("en-US", { style: 'currency', currency: 'USD' }).format(numero);
    retorno = retorno.split(".");
    //console.log(retorno);
    return retorno[0];
-    
 }
 
 function retirarFormatoPesos(numero){
@@ -1011,17 +1018,22 @@ function cambioASOV(){
 
 
 function informacionMarcas() {
-    var todo = "JOHNNIE WALKER;Cerveza;2;2,8,10,5|GORDONS;Cerveza;2;2,8,10,5|SMIRNOFF;Destilado Nacional;1;4,5,6,8,10,2|BLACK & WHITE;Destilado Nacional;1;4,5,6,8,10,2|BLACK & WHITE;Cerveza;1;4,5,6,8,10,2";
+    var todo = "JOHNNIE WALKER;Cerveza;2;2,8,10,5|GORDONS;Cerveza;2;2,8,10,5|SMIRNOFF;Destilado Nacional;1;4,5,6,8,10,2|BLACK & WHITE;Destilado Nacional;1;4,5,6,8,10,2|BLACK & WHITE;Cerveza;1;4,5,6,8,10,2|OLD PARR;Destilado Importado;2;4|OLD PARR;Cerveza;2;2,4|JOHNNIE WALKER;Destilado Importado;2;4|JOHNNIE WALKER;Destilado Nacional;2;4|HAIG CLUB;Cerveza;2;2,8,10";
     return todo;
 }
 function informacionServicios(){
-    var info = "JOHNNIE WALKER;JOHNNIE RED & LEMON;1,2,3;- Llene el vaso con cubos de hielo <br> - Vierta 50 ml de Johnnie Walker Red Label <br>- Llene hasta el tope con gaseosa de lima <br>- Decore con una rodaja de limón <br>;Cerveza;3;2,8,10,5";
+    var info = "JOHNNIE WALKER;JOHNNIE RED & LEMON;1,2,3;- Llene el vaso con cubos de hielo <br> - Vierta 50 ml de Johnnie Walker Red Label <br>- Llene hasta el tope con gaseosa de lima <br>- Decore con una rodaja de limón <br>;Cerveza;3;2,8,10,5,4";
     var info2 = "GORDONS;GORDONS TONIC;4,5,3;- En un vaso largo o copa globo agrega hielo hasta el tope <br>- Luego agregue 50 ml de Gin Gordon's London Dry y <br>complete con agua tónica <br>- Decore con una rodaja de limón <br>;Cerveza;3;2,8,10,5";
     var info3 = "SMIRNOFF;BOTLL SMIRNOFF X1;6;;Destilado Nacional;3;4,5,6,8,10,2";
     var info4 = "BLACK & WHITE;BOTLL B&W;7;;Destilado Nacional;1;4,5,6,8,10,2";
-     var info5 = "BLACK & WHITE;TRAGO B&W;7;;Cerveza;1;4,5,6,8,10,2";
+    var info5 = "BLACK & WHITE;TRAGO B&W;7;;Cerveza;1;4,5,6,8,10,2";
+    var info6 = "OLD PARR;BOTLL OLD PARR;8;;Destilado Importado;2;4";
+    var info7 = "OLD PARR;OLD PARR BEER;8,9,3;;Cerveza;2;2,4";
+    var info8 = "JOHNNIE WALKER;BOTLL JOHNNIE WALKER RED;1;;Destilado Nacional;2;4";
+    var info9 = "JOHNNIE WALKER;BOTLL JOHNNIE WALKER RED;1;;Destilado Importado;2;4";
+    var info10 = "HAIG CLUB;H.O.T;10,5,11;;Cerveza;2;2,8,10";
 
-    var completo = [info,info2 , info3 , info4, info5];
+    var completo = [info,info2 , info3 , info4, info5,info6,info7,info8,info9,info10];
     return completo;
 }
 
